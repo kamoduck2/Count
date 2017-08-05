be5190e19b12baa7ff7c8b3ed7e87c85f782a8af
kamoni <mt99peacefulkoala@icloud.com>
2017-08-05 15:59:24 +0900

    最初のcommit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number < 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number < 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func mult() {
+        number = number * number
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number < 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func divi() {
+        number = number / 2
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }else if number < 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+        label.textColor = UIColor.white
+    }
 }
 
